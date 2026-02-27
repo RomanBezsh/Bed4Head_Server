cb45c87c213d1c17600348cbabb30728cec4fa5f
RomanPro <RomanPro@PC>
2026-02-27 18:05:24 +0200

    innit commit

---
+++ Bed4Head.BLL/DTO/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bed4Head.BLL.DTO
+{
+    public class UserDTO
+    {
+    }
+}
---
+++ Bed4Head_Server/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bed4Head_Server.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
