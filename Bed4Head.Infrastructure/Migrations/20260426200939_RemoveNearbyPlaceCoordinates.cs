ab2ec16af242d84c324abc6b929b4a022346c5bf
Roman Bezshyiko <antikys189@gmail.com>
2026-05-01 00:48:53 +0300

    fixed

---
+++ Bed4Head.Infrastructure/Migrations/20260426200939_RemoveNearbyPlaceCoordinates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bed4Head.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNearbyPlaceCoordinates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "NearbyPlaces");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "NearbyPlaces");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "NearbyPlaces",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "NearbyPlaces",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
